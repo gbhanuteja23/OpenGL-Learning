d6d061aeb776a6c0a32ca5fbd799aabbae150b41
G Bhanuteja <45899803+gbhanuteja23@users.noreply.github.com>
2025-10-09 17:33:50 +0530

    Changes done in Main.cpp, changes to vertices[] amd indices[] and also modified code so that model, view, projection matrices are added for 3D effect

--- OpenGLProj3/Main.cpp
+++ OpenGLProj3/Main.cpp
@@ -3,6 +3,9 @@
 #include<GLFW/glfw3.h>
 #include<stb/stb_image.h>
 #include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
 
 #include "ShaderClass.h"
 #include "VAO.h"
@@ -13,22 +16,32 @@
 using namespace std; 
 
 
+const unsigned int width = 800;
+const unsigned int height = 800; 
+
+
 //Vertices coordinates: Each vertex has 3 position floats (x, y, z) + 3 color floats (r, g, b)
 GLfloat vertices[] =
 {
-	// Positions								   // Colors (R, G, B)
-	-0.5f, -0.5f, 0.0f,							   1.0f, 0.0f, 0.0f,	 0.0f, 0.0f,  		// Lower left corner
-	-0.5f, 0.5f,  0.0f,								0.0f, 1.0f, 0.0f,	 0.0f, 1.0f,		// Upper left corner
-	 0.5f, 0.5f, 0.0f,								0.0f, 0.0f, 1.0f,	 1.0f, 1.0f,		// Upper right corner
-	 0.5f, -0.5f, 0.0f,								1.0f, 1.0f, 1.0f,	 1.0f, 0.0f			// Lower right corner
+	// Positions			// Colors (R, G, B)     //TexCoord
+	-0.5f, 0.0f, 0.5f,		0.83f, 0.70f, 0.44f,		0.0f, 0.0f,  		// Lower left corner
+	-0.5f, 0.0f,  -0.5f,	0.83f, 0.70f, 0.44f,		1.0f, 0.0f,		// Upper left corner
+	 0.5f, 0.0f, -0.5f,		0.83f, 0.70f, 0.44f,		1.0f, 1.0f,		// Upper right corner
+	 0.5f, 0.0f, 0.5f,		0.83f, 0.70f, 0.44f,		0.0f, 1.0f,			// Lower right corner
+	 0.0f, 0.8f, 0.0f,		0.82f, 0.86f, 0.44f,		0.5f, 1.0f
+
 };
 
 
 // Index buffer — defines which vertices make each triangle
 GLuint indices[] =
 {
-	0, 2, 1,	//Upper triangle
-	0, 3, 2     //Lower triangle
+	0, 1, 2,	//Upper triangle
+	0, 2, 3,     //Lower triangle
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
 };
 
 int main()
@@ -64,7 +77,7 @@ int main()
 
 	//Specify the viewport of OpenGL in the window
 	//In this case the viewport goes from x=0, y=0, to x=800, y=800
-	glViewport(0, 0, 800, 800); 
+	glViewport(0, 0, width, height); 
 
 	// Create the shader program using vertex and fragment shaders
 	Shader ShaderProgram("default.vert", "default.frag"); 
@@ -93,21 +106,54 @@ int main()
 
 	//Texture
 
-	Texture woodenTex("saranda.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
+	Texture woodenTex("wooden-texture.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
 	woodenTex.texUnit(ShaderProgram, "tex0", 0);
+
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime(); 
+
+	glEnable(GL_DEPTH_TEST);
 
 	//Main while/Render loop
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);		// Background color
-		glClear(GL_COLOR_BUFFER_BIT);					// Clear frame buffer
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);							// Background color
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);					// Clear frame buffer
 
 		//Tell OpenGL which Shader Program we want to use 
 		ShaderProgram.Activate(); 
 
+		double crntTime = glfwGetTime(); 
+
+		if (crntTime - prevTime >= 1.0f / 60.0f)
+		{
+			rotation += 0.5f;
+			prevTime = crntTime; 
+		}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f)); 
+
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f); 
+
+		int modelLoc = glGetUniformLocation(ShaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model)); 
+
+		int viewLoc = glGetUniformLocation(ShaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+		int projLoc = glGetUniformLocation(ShaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
 		// Set scale value
 		glUniform1f(uniID, 0.2f); 
 
+
+		glActiveTexture(GL_TEXTURE0);
 		// Binds texture so that is appears in rendering
 		woodenTex.Bind();
 
@@ -115,7 +161,7 @@ int main()
 		VAO1.Bind(); 
 
 		//Draw the triangle using the GL_TRIANGLES primitive 
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); 
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);				// Display the rendered frame
 
 		//Take care of all GLFW events
